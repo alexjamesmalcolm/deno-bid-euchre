99aa18f8694ea960ad94a4c240c3d5c57ed91eda
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-18 22:12:22 -0400

    Created mod.ts file

---
+++ mod.ts
@@ -0,0 +1,8 @@
+export {
+  chooseOption,
+  determineIfPhaseIsLegal,
+  getOptions,
+  isLegalOption,
+  startGame,
+} from "./index.ts";
+export { Phase, Option, PlayerPosition, LobbyPlayer } from "./definitions.ts";
